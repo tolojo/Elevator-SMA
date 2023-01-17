779fadda38e5056ebda0d18fd977b568e526d0b0
tolojo <tomasferreira91@gmail.com>
2023-01-17 20:34:52 +0000

    Agencias criadas

--- Risk/src/playerAgent.java
+++ Risk/src/playerAgent.java
@@ -1,3 +1,22 @@
-public class playerAgent {
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+public class playerAgent extends Agent{
     
+  public void setup() {
+    DFAgentDescription dfd = new DFAgentDescription();
+    dfd.setName(getAID());
+    ServiceDescription sd = new ServiceDescription();
+    sd.setType("riskGame");
+    sd.setName(getLocalName() + "-gamePlayer");
+    dfd.addServices(sd);
+    try {
+      DFService.register(this, dfd);
+    } catch (FIPAException fe) {
+      fe.printStackTrace();
+    }
+  }
 }
